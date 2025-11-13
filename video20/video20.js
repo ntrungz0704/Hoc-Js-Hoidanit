bbe5feb837b7bf90459cd6f61dbc90ca483bd1ea
Thanh Trung <ntrungz0704@gmail.com>
2025-11-13 23:59:31 +0700

    toan-tu-video20

---
+++ video20/video20.js
@@ -0,0 +1,46 @@
+// #9 toán tử trong js
+
+/*
+  +
+  -
+  *
+  /
+  %
+  ** lũy thừa
+  ++ tăng 1
+  -- giảm 1
+*/
+
+console.log("video 20");
+
+let a = 5;
+const b = a++;
+// const c = a--;
+
+console.log(`a = ${a}`);
+console.log(`b = ${b}`);
+// console.log(`c = ${c}`);
+
+// toán tử so sánh
+/*
+  >
+  <
+  >=
+  <=
+  == (lỏng)
+  === (chặt) | recommend
+  !=
+  !==
+*/
+
+const c = 5;
+const d = "5";
+console.log(c == d);
+console.log(c === d);
+
+// toán tử logic
+/*
+  &&
+  ||
+  !
+*/
