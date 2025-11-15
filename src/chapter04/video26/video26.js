e41bfa8314248befa830bba53d7c5ba36cf2cd5d
Thanh Trung <ntrungz0704@gmail.com>
2025-11-15 20:49:20 +0700

    cau-dieu-kien

---
+++ src/chapter04/video26/video26.js
@@ -0,0 +1,24 @@
+// #14 câu điều kiện if / else / else if
+console.log("video 26");
+
+const age = 12;
+
+if (age > 18) {
+  // true
+  console.log("bạn có thể xem video");
+} else {
+  console.log("bạn không thể xem video");
+}
+
+// score a, b, c, d, f, giỏi, khá, trung bình
+let score = 8;
+
+if (score >= 9) {
+  console.log("giỏi");
+} else if (score >= 8) {
+  console.log("khá");
+} else if (score >= 7) {
+  console.log("trung bình");
+} else {
+  console.log("yếu");
+}
