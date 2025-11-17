a6381ce92b238899ae32ef1cd202f7d31d67e450
ntrungz0704 <ntrungz0704@gmail.com>
2025-11-17 22:15:28 +0700

    array-object

---
+++ src/chapter05/video37/video37.js
@@ -0,0 +1,13 @@
+// #24 giới thiệu mảng array và đối tượng object
+
+console.log("video 37");
+
+const names = ["trung", "ntrungz", "cún"];
+console.log(names);
+console.log(typeof names);
+
+const others = ["ntrungz07", [1, 2, 3]]; // ít khi code như vậy -
+
+console.log(others);
+
+// và có thể chứa được nhiều kiểu dữ liệu khác nhau
