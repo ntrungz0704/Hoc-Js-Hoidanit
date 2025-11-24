23ef19fef958ee31da9b8aa3418c42a7c7cc3a8d
Thanh Trung <ntrungz0704@gmail.com>
2025-11-24 22:43:48 +0700

    handle-events

---
+++ src/chapter06/video50/video50.js
@@ -0,0 +1,10 @@
+// #36 xử lý sự kiện events
+console.log("video 50");
+
+const element = document.querySelector("#myBtn");
+
+console.log(element);
+
+function hanldeClickBtn() {
+  console.log("you click a button");
+}
