7e484d6556f85d679d09e66077b1c0fcaa2947d4
Thanh Trung <ntrungz0704@gmail.com>
2025-11-24 22:42:58 +0700

    access-element-html-in-dom

---
+++ src/chapter06/video49/video49.js
@@ -0,0 +1,14 @@
+// #35 truy cập phần tử HTML trong DOM
+
+console.log("video 49");
+console.log("\n");
+const ele = document.getElementById("username");
+console.log(ele);
+console.log("\n");
+
+const myDiv = document.querySelector(".ntrungz");
+console.log(myDiv);
+console.log("\n");
+
+const allMyClass = document.querySelectorAll(".ntrungz");
+console.log(allMyClass);
