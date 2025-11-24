05ab6cb82ba7c790cbdd542de0c60d1f2f31b48a
Thanh Trung <ntrungz0704@gmail.com>
2025-11-24 22:46:15 +0700

    alert

---
+++ src/chapter06/video54/video54.js
@@ -0,0 +1,9 @@
+// #40 alert
+
+console.log("video 54");
+
+const myBtnElement = document.querySelector(".myBtn");
+
+myBtnElement.addEventListener("click", () => {
+  alert("Cửa sổ đã được mở ra");
+});
